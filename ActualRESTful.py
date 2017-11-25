c76c234f57e2f83fbf630019a5ad26f8070afcc6
Rowan Sutton <sutton.r11@gmail.com>
2017-11-25 11:31:33 +0000

    multiple argument parsing on POST

--- ActualRESTful.py
+++ ActualRESTful.py
@@ -18,6 +18,8 @@ class fileListApi(Resource):
 
         # For every value coming in JSON, you need an argument
         self.reqparser.add_argument('filename', type=str, location = 'json')  # Repeat for multiple variables
+        self.reqparser.add_argument('data', type=str, location='json')  # Repeat for multiple variables
+        self.reqparser.add_argument('version', type=int, location='json')  # Repeat for multiple variables
         # e.g to add a client_ID value do:
         # self.reqparser.add_argument('client_ID', type=str, location = 'json')  # Repeat for multiple variables
 
@@ -34,12 +36,6 @@ class fileListApi(Resource):
         self.server.files.append(f)
         return {'file': f}
 
-    def put(self):  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        pass  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-    def delete(self):  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        pass  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
 #  Created a route at /files with an endpoint called files
 api.add_resource(fileListApi, "/filedir", endpoint="filelist")  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
@@ -51,9 +47,10 @@ class fileApi(Resource):
         self.server = fileS
         super(fileApi, self).__init__()  # Initialising the Resource class
         self.reqparser = reqparse.RequestParser()  # Init a request parser
-
         # For every value coming in JSON, you need an argument
         self.reqparser.add_argument('filename', type=str, location='json')  # Repeat for multiple variables
+        self.reqparser.add_argument('data', type=str, location='json')  # Repeat for multiple variables
+        self.reqparser.add_argument('version', type=int, location='json')
         # self.reqparser.add_argument('Client_ID', type=str, location = 'json')  # Repeat for multiple variables
 
     def get(self, filename):
@@ -61,24 +58,28 @@ class fileApi(Resource):
         if len(f) == 0:
             return {'success': False}  # Not in the list
         f = f[0]  # Take first element of f (should only be one)
-        datatosend = self.server.filedata[f['filename']]
+        print(f)
 
-        return {"filedata": datatosend}  # f['filename'] to just get the name of the file
+        return f # f['filename'] to just get the name of the file
         # return {"Hello": "World"}  # Automatically converted to JSON since returning a dictionary
 
+    #  FILENAME IS PASSED FROM ENDPOINT <STRING>
     def put(self, filename):
-        f = [f for f in self.server.files if f['filename'] == filename]
-        if len(f) == 0:
-            return {'success': False}
-        f = f[0]  # Only take the first value
         args = self.reqparser.parse_args()  # args is a list containing
-        for k, v in args.items():
-            if v != None:
-                f[k] = v
-        return {'file': f}
-
-    def delete(self, filename):  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        pass  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        print("hi")
+        print(args['version'])
+        # f = [f for f in self.server.files if f['filename'] == filename]
+        # if len(f) == 0:
+        #     return {'success': False}
+        # f = f[0]  # Only take the first value
+        # args = self.reqparser.parse_args()  # args is a list containing
+        # print(args)
+        # for k, v in args.items():
+        #     if v != None:
+        #        print(v)
+        #         f[k] = v
+        # return {'file': f}
+        return {"success":True}
 
 #  Created a route at /files/'input string' with an endpoint called files
 #  The 'input string' is taken as the value "name"
@@ -95,11 +96,13 @@ class fileServer():
         with open("file2.txt", "r") as myfile:
             data2 = myfile.readlines()
 
-        self.filedata = {'file1':data1, 'file2':data2}
+        counter = 0
+        counter2 = 0
 
         print(data1)
         #  Init files
-        self.files = [{'filename':'file1'},{'filename':'file2'}]
+        self.files = [{'filename':'file1', "data":data1, "version":counter},
+                      {'filename':'file2', "data":data2, "version":counter2}]
 
 if __name__ == "__main__":  ## XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
     fileS = fileServer()  # Fill fileS with the init values of class fileServer
--- basicClient.py
+++ basicClient.py
@@ -8,6 +8,7 @@ def run():
     r = requests.get('http://localhost:5000/filedir/file1')
     r_text = r.json()
     print(r_text)
+    r = requests.put('http://localhost:5000/filedir/file1', json={'filename':'file2','version':10})  ## NEED JSON
 
 if __name__ == "__main__":
     run()
