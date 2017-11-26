4bc7a430fbbd293be7cddae77cf3404b0eee5268
Rowan Sutton <sutton.r11@gmail.com>
2017-11-26 16:50:54 +0000

    Client can now locate the server containing the file. Need to include case for file not on system

--- Directory Service (part 3 of 4)/basicClient.py
+++ Directory Service (part 3 of 4)/basicClient.py
@@ -5,49 +5,52 @@ import requests, json, clientLibrary
 # r_text = r.json()
 
 def run():
-    ipAddress = input("Enter the IP of the fileserver: ")
-    portNumber = input("Enter the port number of the fileserver: ")
-    print("IP:{} Port:{}".format(ipAddress,portNumber))
-    running = True
-    while running:
-        userChoice = input("Enter the number for the specified action\n"
-                           "1. List files, their versions and their data\n"
-                           "2. Read a file\n"
-                           "3. Write to an existing file\n"
-                           "4. Create a new file\n"
-                           "5. Delete an existing file\n"
-                           "0. Exit the client application\n")
-        if userChoice == '1':
-            clientLibrary.listFiles(ipAddress,portNumber)
-
-        elif userChoice == '2':
-            userFile = input("Enter the file name: ")  # must include file extension
-            fileRecv = clientLibrary.getFile(ipAddress, portNumber, userFile)
-            if fileRecv != -1:
-                clientLibrary.printFile(fileRecv)
-
-        elif userChoice == '3':
-            userFile = input("Enter the file name: ")
-            fileRecv = clientLibrary.getFile(ipAddress, portNumber, userFile)
-            if fileRecv != -1:
-                clientLibrary.printFile(fileRecv)
-                dataToWrite = input("Type the text you want to write to the file: ")
-                clientLibrary.editFile(ipAddress, portNumber, fileRecv, dataToWrite)
-            else:
-                print("File not found\n")
-
-        elif userChoice == '4':
-            userFile = input("Enter the new file name: ")
-            userData = input("Enter the new data for the file: ")
-            clientLibrary.createFile(ipAddress, portNumber, userFile, userData)
-
-        elif userChoice == '5':
-            userFile = input("Enter the file name to delete: ")
-            clientLibrary.deleteFile(ipAddress, portNumber, userFile)
-
-        elif userChoice == '0':
-            print("Ending client application")
-            running = False
+    r = requests.get('http://localhost:5000/filedir/2file1.txt')
+    json_data = json.loads(r.text)  # JSON to dict
+    print(json_data)
+    # ipAddress = input("Enter the IP of the fileserver: ")
+    # portNumber = input("Enter the port number of the fileserver: ")
+    # print("IP:{} Port:{}".format(ipAddress,portNumber))
+    # running = True
+    # while running:
+    #     userChoice = input("Enter the number for the specified action\n"
+    #                        "1. List files, their versions and their data\n"
+    #                        "2. Read a file\n"
+    #                        "3. Write to an existing file\n"
+    #                        "4. Create a new file\n"
+    #                        "5. Delete an existing file\n"
+    #                        "0. Exit the client application\n")
+    #     if userChoice == '1':
+    #         clientLibrary.listFiles(ipAddress,portNumber)
+    #
+    #     elif userChoice == '2':
+    #         userFile = input("Enter the file name: ")  # must include file extension
+    #         fileRecv = clientLibrary.getFile(ipAddress, portNumber, userFile)
+    #         if fileRecv != -1:
+    #             clientLibrary.printFile(fileRecv)
+    #
+    #     elif userChoice == '3':
+    #         userFile = input("Enter the file name: ")
+    #         fileRecv = clientLibrary.getFile(ipAddress, portNumber, userFile)
+    #         if fileRecv != -1:
+    #             clientLibrary.printFile(fileRecv)
+    #             dataToWrite = input("Type the text you want to write to the file: ")
+    #             clientLibrary.editFile(ipAddress, portNumber, fileRecv, dataToWrite)
+    #         else:
+    #             print("File not found\n")
+    #
+    #     elif userChoice == '4':
+    #         userFile = input("Enter the new file name: ")
+    #         userData = input("Enter the new data for the file: ")
+    #         clientLibrary.createFile(ipAddress, portNumber, userFile, userData)
+    #
+    #     elif userChoice == '5':
+    #         userFile = input("Enter the file name to delete: ")
+    #         clientLibrary.deleteFile(ipAddress, portNumber, userFile)
+    #
+    #     elif userChoice == '0':
+    #         print("Ending client application")
+    #         running = False
 
         # clientLibrary.listFiles('127.0.0.1', 5000)
         # firstFile = clientLibrary.getFile('localhost', 5000, 'file1.txt')
--- Directory Service (part 3 of 4)/directoryServer.py
+++ Directory Service (part 3 of 4)/directoryServer.py
@@ -5,24 +5,92 @@ import requests, json
 app = Flask(__name__)
 api = Api(app)
 
+class getFileLocation(Resource):
+    def __init__(self):  # Upon initialisation of the class
+        global fileS  # Init the global server
+        self.server = fileS  # Init the global server
+        super(getFileLocation, self).__init__()  # Initialising the Resource class
+        self.reqparser = reqparse.RequestParser()
+
+        # For every value coming in JSON, you need an argument
+        self.reqparser.add_argument('filename', type=str, location = 'json')  # Repeat for multiple variables
+        self.reqparser.add_argument('data', type=str, location='json')
+        self.reqparser.add_argument('version', type=int, location='json')
+
+    def get(self, filename):
+
+        # Need to update file lists in case anything has changed
+        self.server.serverInfo['fileServer1']['serverFiles'] = []  # reset file lists for checking files
+        self.server.serverInfo['fileServer2']['serverFiles'] = []  # reset file lists for checking files
+        # file server 1 on port 5001
+        # file server 2 on port 5002
+        location = 'http://{}:{}/filedir'.format(self.server.serverInfo['fileServer1']['ip'],
+                                                 self.server.serverInfo['fileServer1']['port'])
+        r = requests.get(location)
+        json_data = json.loads(r.text)  # JSON to dict
+        print("FILESERVER 1 FILES: {}".format(json_data))
+
+        for x in json_data:
+            self.server.serverInfo['fileServer1']['serverFiles'].append(x['filename'])
+
+        location = 'http://{}:{}/filedir'.format('localhost', 5002)
+        r = requests.get(location)
+        json_data = json.loads(r.text)  # JSON to dict
+        print("FILESERVER 2 FILES: {}".format(json_data))
+
+        for x in json_data:
+            self.server.serverInfo['fileServer2']['serverFiles'].append(x['filename'])
+
+        # Now that the file lists are up to date, can locate what server the file is on
+        for x in self.server.serverInfo['fileServer1']['serverFiles']:
+            if x == filename:
+                return {'ip':self.server.serverInfo['fileServer1']['ip'],
+                        'port':self.server.serverInfo['fileServer1']['port'],
+                        'filename':filename}
+
+        for x in self.server.serverInfo['fileServer2']['serverFiles']:
+            if x == filename:
+                return {'ip':self.server.serverInfo['fileServer2']['ip'],
+                        'port':self.server.serverInfo['fileServer2']['port'],
+                        'filename':filename}
+
+        print(self.server.serverInfo)
+        pass
+        # return {"Hello": "World"}  # Automatically converted to JSON since returning a dictionary
+
+    #  Creating new file data and adding it to the list
+    def post(self):
+        pass
+
+#  Created a route at /files with an endpoint called files
+api.add_resource(getFileLocation, "/filedir/<string:filename>", endpoint="file")
 
 
 class fileServer():
     def __init__(self):
         # If time, create directory with files inside and iterate to fill self.files
         # instead of explicit initialisation of files
-        self.fileMap = []  # Stores all file data
+        self.serverInfo = {'fileServer1':{'ip':'localhost', 'port':5001, 'serverFiles':[]},
+                           'fileServer2': {'ip': 'localhost', 'port': 5002, 'serverFiles':[]}}
         # file server 1 on port 5001
         # file server 2 on port 5002
-        location = 'http://{}:{}/filedir'.format('localhost', 5001)
+        location = 'http://{}:{}/filedir'.format(self.serverInfo['fileServer1']['ip'], self.serverInfo['fileServer1']['port'])
         r = requests.get(location)
         json_data = json.loads(r.text)  # JSON to dict
         print("FILESERVER 1 FILES: {}".format(json_data))
+
+        for x in json_data:
+            self.serverInfo['fileServer1']['serverFiles'].append(x['filename'])
 
         location = 'http://{}:{}/filedir'.format('localhost', 5002)
         r = requests.get(location)
         json_data = json.loads(r.text)  # JSON to dict
         print("FILESERVER 2 FILES: {}".format(json_data))
+
+        for x in json_data:
+            self.serverInfo['fileServer2']['serverFiles'].append(x['filename'])
+
+        print(self.serverInfo)
 
 if __name__ == "__main__":
     fileS = fileServer()  # Fill fileS with the init values of class fileServer
